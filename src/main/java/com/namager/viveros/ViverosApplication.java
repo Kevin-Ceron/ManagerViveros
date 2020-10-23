ba3a964de1d59e7a4a4ff15166e42e9a75956877
Kevin-Ceron <49286038+Kevin-Ceron@users.noreply.github.com>
2020-10-22 20:47:21 -0500

    Creacion de crud para Gestor

---
+++ src/main/java/com/namager/viveros/ViverosApplication.java
@@ -0,0 +1,13 @@
+package com.namager.viveros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViverosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViverosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namager/viveros/ViverosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namager.viveros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViverosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
