5aeb11e9167689e019aac9ac9a93c682fe8ad755
Kevin-Ceron <49286038+Kevin-Ceron@users.noreply.github.com>
2020-10-30 19:28:31 -0500

    Metodo listar para tabla Viveros Completo

---
+++ src/main/java/com/manager/viveros/service/ViveroServiceImp.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.manager.viveros.service;
+
+import com.manager.viveros.model.Vivero;
+import com.manager.viveros.repository.ViveroRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author alfar
+ */
+@Service
+public class ViveroServiceImp implements ViveroService{
+    
+    @Autowired
+    private ViveroRepository repository; 
+
+    @Override
+    public List<Vivero> Listar() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Vivero listarId(int id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Vivero add(Vivero v) {
+        return repository.save(v);
+    }
+
+    @Override
+    public Vivero edit(Vivero v) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Vivero delete(int id) {
+        Vivero v = repository.findById(id);
+        if (v != null){
+            repository.delete(v);
+        }
+        return v;
+    }
+    
+}
